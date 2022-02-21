dd57c0b1936d20c373c2a3a20a06fae565abf33f
Natalia Mosyuk <natlia.m21@gmail.com>
2022-02-21 15:39:06 +0200

    add new components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-basics';
+  posts: Post[] = [
+    {title: "Angular componets learning", text: "Angular block", id: 1},
+    {title: "Next angular components", text: "Angular component block 2", id: 2}
+  ]
 }
